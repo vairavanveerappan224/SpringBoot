ba712152e72e9c165094efc03ba11369fadb43e8
vairavanveerappan224 <vairavanveerappan2004@gmail.com>
2025-12-09 11:07:29 +0530

    added product controller with apiresponse and with images and date

--- applicationSpring/src/main/java/com/example/applicationSpring/repository/ProductRepository.java
+++ applicationSpring/src/main/java/com/example/applicationSpring/repository/ProductRepository.java
@@ -1,12 +1,23 @@
 package com.example.applicationSpring.repository;
 
 import com.example.applicationSpring.entity.ProductEntity;
+import org.antlr.v4.runtime.atn.SemanticContext;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.swing.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity,Integer> {
     List<ProductEntity> findBySubCategoryId(int subCategoryId);
     void deleteBySubCategoryId(int subCategoryId);
+    List<ProductEntity> findByProductNameContainingIgnoreCase(String keyword);
+    @Query("SELECT p FROM ProductEntity p WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
+            "LOWER(p.subCategory.subCategoryName) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
+            "LOWER(p.subCategory.category.name) LIKE LOWER(CONCAT('%', :text, '%'))")
+    List<ProductEntity> filter(String text);
 
+    List<ProductEntity> findByPriceBetween(Double min, Double max);
+    List<ProductEntity> findByTimeStampBetween(LocalDateTime start, LocalDateTime end);
 }
